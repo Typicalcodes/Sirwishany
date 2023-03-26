e4e08275412b977081469c6adb8db8adc9c02b6d
Typicalcodes <100959820+Typicalcodes@users.noreply.github.com>
2023-03-26 09:16:51 +0530

    Here is my commit

--- sirwishany/src/App.js
+++ sirwishany/src/App.js
@@ -1,19 +1,22 @@
-
-import './App.css';
-import Navbar2 from './Components/Navbar2';
-
-import Itembox from './Components/Items Box/Itembox';
-import Login from './Components/Login';
+import "./App.css";
+import Navbar2 from "./Components/Navbar2";
+import img from "../src/Plumber.png";
+import Itembox from "./Components/Items Box/Itembox";
+import Login from "./Components/Login";
 import { useSelector } from "react-redux";
 function App() {
   const logn = useSelector((state) => state.loginopen);
-  console.log(logn)
+  console.log(logn);
   return (
-    <div className='relative'>
-     { logn && <Login /> }
+    <div className="relative bg-gray-100 ">
+      {logn && <Login />}
 
       <Navbar2 />
-      <Itembox/>
+      <Itembox />
+      <div className="container relative ">
+        <img src={img} className="rounded-xl absolute w-auto px-[8px]  object-fit items-center justify-center" alt="" />
+      <span className="absolute top-2 right-6 font-merri font-bold text-xl text-white ">Professionals  <br/> Plumbers</span>
+      </div>
     </div>
   );
 }
